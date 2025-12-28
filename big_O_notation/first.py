00eb781c370e2b6ef01f45da6f1512cc792adbae
SanjarKeySkills <forest2004@mail.ru>
2025-12-29 00:25:18 +0600

    auto commit Mon, Dec 29, 2025 12:25:18 AM

--- big_O_notation/first.py
+++ big_O_notation/first.py
@@ -109,3 +109,21 @@ def power(x, n):
 
 print(power(2, 10))  # 1024
 
+# --------------
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+# Пример использования
+arr = [1, 3, 5, 7, 9, 11, 13]
+print(binary_search(arr, 7))  # 3
+print(binary_search(arr, 10)) # -1
