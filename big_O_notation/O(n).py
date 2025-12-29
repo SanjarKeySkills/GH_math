3ccafa85a10e3929a534111799f3e8540fe3247a
SanjarKeySkills <forest2004@mail.ru>
2025-12-29 23:59:54 +0600

    auto commit Mon, Dec 29, 2025 11:59:54 PM

--- big_O_notation/O(n).py
+++ big_O_notation/O(n).py
@@ -1,2 +1,8 @@
 def firnd_afetr_index(arr, target, start_from = 0):
-    
+    for i, num in enumerate(arr):
+        if i >= start_from and num == target:
+            return 1
+    return -1
+
+arr = [10, 20, 30, 20, 40]
+print()
