34499548de3aa128da8639d3366ba41d3ae42cda
SanjarKeySkills <forest2004@mail.ru>
2025-12-30 00:01:09 +0600

    auto commit Tue, Dec 30, 2025 12:01:09 AM

--- big_O_notation/O(n).py
+++ big_O_notation/O(n).py
@@ -1,8 +1,8 @@
-def firnd_afetr_index(arr, target, start_from = 0):
+def find_after_index(arr, target, start_from = 0):
     for i, num in enumerate(arr):
         if i >= start_from and num == target:
             return 1
     return -1
 
 arr = [10, 20, 30, 20, 40]
-print()
+print(find_after_index(arr, 20, 2ss))
