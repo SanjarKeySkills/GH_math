fdad76531d56905aa10bd8c132177a13b6212b2c
SanjarKeySkills <forest2004@mail.ru>
2025-12-30 23:57:06 +0600

    auto commit Tue, Dec 30, 2025 11:57:06 PM

--- big_O_notation/script_o_n.py
+++ big_O_notation/script_o_n.py
@@ -8,3 +8,15 @@ def find_after_index(arr, target, start_from = 0):
 
 arr = [10, 20, 30, 20, 40]
 print(find_after_index(arr, 20, 2))
+
+# ----O(log N)------------
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right)//2 #looking for middle
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1 # search in right side
+        else:
+            right = mid - 1 # search in the left side
