7e9cee69bf0c06e3047c5d29f2b6aafef263e5f3
SanjarKeySkills <forest2004@mail.ru>
2026-01-03 00:45:51 +0600

    auto commit Sat, Jan  3, 2026 12:45:51 AM

--- big_O_notation/script_o_log_n.py
+++ big_O_notation/script_o_log_n.py
@@ -29,6 +29,7 @@ def merge(left, right):
     while i < len(left) and j < len(right):
         if left[i] <= right[j]:
             print(len(left))
+            print(len(right))
             result.append(left[i])
             i += 1
         else:
