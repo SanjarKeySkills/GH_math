8c7383c0a9268af148b7bf37fdae8cd27566c1a0
SanjarKeySkills <forest2004@mail.ru>
2026-01-03 00:04:08 +0600

    auto commit Sat, Jan  3, 2026 12:04:08 AM

--- big_O_notation/script_o_log_n.py
+++ big_O_notation/script_o_log_n.py
@@ -1,12 +1,12 @@
 arr = [1, 2, 3, 4, 5, 6, 7]
 dict_from_list = {i: val for i, val in enumerate(arr)}
-print(dict_from_list)
+# print(dict_from_list)
 
 #-------------------
 
 students =  ["Ann", "Boris", "Tanya", "Nathan"]
 students_ids = {id: name for id, name in enumerate(students, start=13)}
-print(students_ids)
+# print(students_ids)
 
 # ---------
 arr = [45, 98, 3, 24, 78, 56, 14, 97, 55, 1, 18, 73]
