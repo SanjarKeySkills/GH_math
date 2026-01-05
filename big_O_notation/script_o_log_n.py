1c6ec37fdf4ae0ec94ca9ec8fad9b31000604b78
SanjarKeySkills <forest2004@mail.ru>
2026-01-05 23:27:30 +0600

    auto commit Mon, Jan  5, 2026 11:27:30 PM

--- big_O_notation/script_o_log_n.py
+++ big_O_notation/script_o_log_n.py
@@ -1,12 +1,12 @@
 arr = [1, 2, 3, 4, 5, 6, 7]
 dict_from_list = {i: val for i, val in enumerate(arr)}
-print(dict_from_list)
+# print(dict_from_list)
 
 #-----------------------------------------
 
 students =  ["Ann", "Boris", "Tanya", "Nathan"]
 students_ids = {id: name for id, name in enumerate(students, start=13)}
-print(students_ids)
+# print(students_ids)
 
 # ----------------------------------------
 arr = [45, 98, 3, 24, 78, 56, 14, 97, 55, 1, 18, 73]
@@ -39,8 +39,8 @@ def merge(left, right):
     return result
 
 sorted_arr = separate(arr)
-print("not sorted arr", arr)
-print("sorted", sorted_arr)
+# print("not sorted arr", arr)
+# print("sorted", sorted_arr)
 
 
 # -------------separate---arrays----sorts------------
